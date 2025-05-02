6e373f0a683f4fdabeb0dcdae9145bfd83bbe50e
Dara Anggi Puspa <154232666+daraanggi@users.noreply.github.com>
2025-05-02 11:25:00 +0700

    Menambahkan file koneksi.php untuk mengoneksikan ke database

---
+++ koneksi.php
@@ -0,0 +1,13 @@
+<?php
+$host = "localhost";
+$user = "root";
+$pass = ""; // kosong kalau pakai XAMPP
+$db   = "todolist_db";
+
+$conn = mysqli_connect($host, $user, $pass, $db);
+
+if (!$conn) {
+    die("Koneksi gagal: " . mysqli_connect_error());
+}
+?>
+
