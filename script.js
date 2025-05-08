e0fc515c060e0c2dde2b14c40464075b38384364
Dara Anggi Puspa <154232666+daraanggi@users.noreply.github.com>
2025-05-09 00:39:44 +0700

    Menambahkan fungsi untuk pengguna agar bisa melihat dan menyembunyikan password

--- script.js
+++ script.js
@@ -1,3 +1,21 @@
 document.addEventListener('DOMContentLoaded', () => {
     console.log("Halaman To-Do List siap!");
-})
+});
+
+function togglePassword() {
+    const passwordField = document.getElementById("password");
+    const toggleIcon = document.getElementById("toggleIcon");
+
+    if (passwordField.type === "password") {
+        passwordField.type = "text";
+        // Password terlihat, jadi tampilkan ikon "mata terbuka"
+        toggleIcon.classList.remove("fa-eye-slash");
+        toggleIcon.classList.add("fa-eye");
+    } else {
+        passwordField.type = "password";
+        // Password disembunyikan, jadi tampilkan ikon "mata tertutup"
+        toggleIcon.classList.remove("fa-eye");
+        toggleIcon.classList.add("fa-eye-slash");
+    }
+}
+
