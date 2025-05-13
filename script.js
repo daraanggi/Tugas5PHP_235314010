5ef4ec6bf1a83ff27a02fee0d4b94dce2a4f2182
Dara Anggi Puspa <154232666+daraanggi@users.noreply.github.com>
2025-05-14 02:14:31 +0700

    Menambahkan komentar

--- script.js
+++ script.js
@@ -2,20 +2,20 @@ document.addEventListener('DOMContentLoaded', () => {
     console.log("Halaman To-Do List siap!");
 });
 
+//Fungsi untuk mengubah visibilitas password
 function togglePassword() {
     const passwordField = document.getElementById("password");
     const toggleIcon = document.getElementById("toggleIcon");
 
     if (passwordField.type === "password") {
         passwordField.type = "text";
-        // Password terlihat, jadi tampilkan ikon "mata terbuka"
+        //Password terlihat, jadi tampilkan ikon "mata terbuka"
         toggleIcon.classList.remove("fa-eye-slash");
         toggleIcon.classList.add("fa-eye");
     } else {
         passwordField.type = "password";
-        // Password disembunyikan, jadi tampilkan ikon "mata tertutup"
+        //Password disembunyikan, jadi tampilkan ikon "mata tertutup"
         toggleIcon.classList.remove("fa-eye");
         toggleIcon.classList.add("fa-eye-slash");
     }
 }
-
